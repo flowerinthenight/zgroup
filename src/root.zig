4e70328e0c5401f44acd5a6caed05c1bf482b668
flowerinthenight <root@flowerinthenight.com>
2024-08-18 14:22:49 +0900

    test cast

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const Sample = packed struct {
+    id: u64 = 1,
+    pos: i64 = -1,
+};
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "gen" {
+    const n = @sizeOf(Sample);
+    std.debug.print("size={d}\n", .{n});
+    const tmp = Sample{};
+    const b = std.mem.asBytes(&tmp);
+    std.debug.print("bytes={any}, ptr={any}, len={any}\n", .{ b, b.ptr, b.len });
+
+    const ptr = @intFromPtr(b);
+    std.debug.print("back={any}\n", .{ptr});
+    const back: *Sample = @ptrFromInt(ptr);
+    std.debug.print("id={any}, pos={any}\n", .{ back.id, back.pos });
 }
