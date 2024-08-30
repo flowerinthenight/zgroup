be0166d73444df835dfe9cfe64d93f90a15a85a1
flowerinthenight <root@flowerinthenight.com>
2024-08-30 13:33:25 +0900

    test: udp client server

--- src/client.zig
+++ src/client.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("client.\n", .{});
+    const port = 8080;
+    std.debug.print("Connecting to :{any}...\n", .{port});
+    const addr = try std.net.Address.resolveIp("127.0.0.1", port);
+    const sock = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM | std.posix.SOCK.CLOEXEC, 0);
+    defer std.posix.close(sock);
+
+    try std.posix.connect(sock, &addr.any, addr.getOsSockLen());
+    _ = try std.posix.write(sock, "hello from client\n");
+    _ = try std.posix.write(sock, "quit\n");
 }
--- src/server.zig
+++ src/server.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("server.\n", .{});
+    const port = 8080;
+    std.debug.print("Starting UDP server on :{any}...\n", .{port});
+    const addr = try std.net.Address.resolveIp("127.0.0.1", port);
+    const sock = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, std.posix.IPPROTO.UDP);
+    defer std.posix.close(sock);
+
+    try std.posix.bind(sock, &addr.any, addr.getOsSockLen());
+    var buf: [1024]u8 = undefined;
+
+    while (true) {
+        const len = try std.posix.recv(sock, &buf, 0);
+        std.debug.print("{d}: {s}", .{ len, buf[0..len] });
+        if (std.mem.eql(u8, buf[0 .. len - 1], "quit")) {
+            break;
+        }
+    }
 }
