504aef8170e1fadc30fbb6ac9faa289f25629090
flowerinthenight <root@flowerinthenight.com>
2024-09-03 17:42:53 +0900

    wip: try scoped logs

--- src/client.zig
+++ src/client.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const print = std.debug.print;
 const Payload = @import("main.zig").Payload;
 
+const log = std.log;
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -16,7 +18,7 @@ pub fn main() !void {
     ptr.primary = true;
 
     const port = 8080;
-    print("Connecting to :{any}...\n", .{port});
+    log.info("Connecting to :{any}...", .{port});
     const addr = try std.net.Address.resolveIp("127.0.0.1", port);
     const sock = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM | std.posix.SOCK.CLOEXEC, 0);
     defer std.posix.close(sock);
@@ -25,10 +27,10 @@ pub fn main() !void {
     try std.posix.connect(sock, &addr.any, addr.getOsSockLen());
     _ = try std.posix.write(sock, std.mem.asBytes(ptr));
     len = try std.posix.recv(sock, buf, 0);
-    print("{d}: reply: id={d}, name=0x{x}\n", .{ len, ptr.id, ptr.name });
+    log.info("{d}: reply: id={d}, name=0x{x}", .{ len, ptr.id, ptr.name });
 
     ptr.id = 0; // this will cause server to quit
     _ = try std.posix.write(sock, std.mem.asBytes(ptr));
     len = try std.posix.recv(sock, buf, 0);
-    print("{d}: reply: id={d}, name=0x{x}\n", .{ len, ptr.id, ptr.name });
+    log.info("{d}: reply: id={d}, name=0x{x}", .{ len, ptr.id, ptr.name });
 }
